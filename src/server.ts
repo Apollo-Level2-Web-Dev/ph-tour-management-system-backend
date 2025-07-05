708b2771ef0f42a2a2b6f41cc16ba1eee96cedde
Samin Israr Ravi <saminisrar1@gmail.com>
2025-07-05 16:37:24 +0600

    server init

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+let server: Server;
+
+
+
+const startServer = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://todoapp:todoapp@cluster0.edawjnd.mongodb.net/tour?retryWrites=true&w=majority&appName=Cluster0")
+
+        console.log("Connected to DB!!");
+
+        server = app.listen(5000, () => {
+            console.log("Server is listening to port 5000");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer()
+
