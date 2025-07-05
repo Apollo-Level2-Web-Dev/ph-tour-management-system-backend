4aef0fb1eb0f9fa0255b6b74b777d582157912e8
Samin Israr Ravi <saminisrar1@gmail.com>
2025-07-05 17:23:14 +0600

    eslint setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
@@ -5,10 +6,9 @@ import app from "./app";
 let server: Server;
 
 
-
 const startServer = async () => {
     try {
-        await mongoose.connect("mongodb+srv://todoapp:todoapp@cluster0.edawjnd.mongodb.net/tour?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://touradmin:touradmin@cluster0.edawjnd.mongodb.net/tour-db?retryWrites=true&w=majority&appName=Cluster0")
 
         console.log("Connected to DB!!");
 
@@ -21,4 +21,66 @@ const startServer = async () => {
 }
 
 startServer()
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+// Unhandler rejection error
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+// Uncaught Exception Error
+// throw new Error("I forgot to handle this local erro")
+
+
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
 
